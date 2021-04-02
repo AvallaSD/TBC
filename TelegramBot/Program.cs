0ea3db4782a67241a34aa486af81e02f5372bfb5
AvallaSD <60776606+AvallaSD@users.noreply.github.com>
2021-04-02 17:45:55 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Telegram.Bot;
+
+namespace TelegramBot
+{
+    class Program
+    {
+        static void Main()
+        {
+            BotClient client = new BotClient("1459208947:AAF4btGXyWkEQIW21eMMs0t38fWtnY6fGyQ");
+            while (true)
+            {
+                Console.WriteLine("Бот работает. Для прекращения введите exit:");
+                if (Console.ReadLine() == "exit")
+                {
+                    break;
+                }
+            }
+        }
+        
+        
+        
+    }
+}
---
+++ TelegtramBotWPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Telegram.Bot.Args;
+using TelegramBot; 
+
+namespace TelegtramBotWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BotClient client = new BotClient("1459208947:AAF4btGXyWkEQIW21eMMs0t38fWtnY6fGyQ");
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventHandler<MessageEventArgs> handler = MessageListener;
+            client.AddListener(handler);
+        }
+        
+        public void MessageListener(object sender, MessageEventArgs e)
+        {
+            MessageBox.Show(e.Message.Text);
+        }
+    }
+}
